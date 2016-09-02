665a41c594572de271103e188a0e7a04099c79c7
divya-kandhasamy <divya1314123@gmail.com>
2016-09-02 10:24:08 +0530

    Create reg.c

---
+++ reg.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int i=0,j,k;
+int a[10];
+printf("Enter the reg.number");
+scanf("%d",&reg);
+while(reg!=0)
+{
+a[i]=reg%10;
+reg=reg/10;
+i++;
+}
+for(j=0;j<i;j++)
+{
+for(k=j+1;k<i;k++)
+{
+if(a[j]==a[k] && a[k]!='\0')
+a[k]='\0';
+printf("%d",a[j]);
+}
+}
+getch();
+}
